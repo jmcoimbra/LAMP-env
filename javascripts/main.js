169ce40809507c0bc7965109f6f34be5cb806855
Juliano M. Coimbra <juliano@jmcoimbra.eng.br>
2015-03-30 18:20:20 -0300

    Initial content and gitignore setup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+// Code from https://css-tricks.com/automatic-table-of-contents/
+var ToC =
+  "<h3>Table of Contents</h3>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h3").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC += "</ul>";
+
+$(".table-of-contents").prepend(ToC);
